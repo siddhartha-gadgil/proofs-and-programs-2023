a102181a2615bbba8704643bb36c0f19deabd6f8
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-11-25 12:16:25 +0530

    docgen added

--- lakefile.lean
+++ lakefile.lean
@@ -13,3 +13,6 @@ lean_lib PnP2023 {
 lean_exe pnP2023 {
   root := `Main
 }
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
