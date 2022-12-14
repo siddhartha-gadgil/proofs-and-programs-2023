670378089752a1559420040d62d933da7e336009
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-14 09:37:13 +0530

    added mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -14,5 +14,8 @@ lean_exe pnP2023 {
   root := `Main
 }
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"@ "master"
+
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
