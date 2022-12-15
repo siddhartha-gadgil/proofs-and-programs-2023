78e9060ee382a8838ea949ed2e716753e5957a7f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-15 11:39:51 +0530

    Odd example and configuration successful

---
+++ PnP2023/Lec_01_04/OddExample.lean
@@ -0,0 +1,15 @@
+import LeanCodePrompts.Translate
+import LeanCodePrompts.CodeActions
+
+/-!
+## A bit of AI
+
+We use `leanaide` for a bit of AI. For more direct use (such as debugging), can use
+
+```lean
+#eval translateViewM "There are infinitely many odd numbers"
+```
+-/
+
+
+/- There are infinitely many odd numbers -/
