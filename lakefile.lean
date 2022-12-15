6432152581b83e158c6996f2ee669c88499f371c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-15 11:14:50 +0530

    lean-aide dependency, builds

--- lakefile.lean
+++ lakefile.lean
@@ -17,5 +17,7 @@ lean_exe pnP2023 {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@ "master"
 
+require leanaide from git "https://github.com/siddhartha-gadgil/LeanAide.git" @ "mathlib4"
+
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
