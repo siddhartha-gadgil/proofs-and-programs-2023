542e0bd84c4021199d0b495381312abd938cfd48
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-22 15:33:55 +0530

    tweak autobuild, CI

--- PnP2023.lean
+++ PnP2023.lean
@@ -1,2 +1,2 @@
-import PnP2023.Lec_01_04.Intro
-import PnP2023.Lec_01_04.OddExample
+
+def hello := "world"
--- mk_imports.sh
+++ mk_imports.sh
@@ -2,3 +2,4 @@
 set -e
 
 find PnP2023 -name "*.lean" | env LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /' > PnP2023.lean
+printf '\ndef hello := "world"\n' > PnP2023.lean
