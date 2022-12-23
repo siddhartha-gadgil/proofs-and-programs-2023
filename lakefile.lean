84d0446936e8b44bbffaf11f1ab63e8a6b15b851
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-23 06:54:38 +0530

    mathlib version with primes

--- lakefile.lean
+++ lakefile.lean
@@ -20,4 +20,4 @@ require mathlib from git
 require leanaide from git "https://github.com/siddhartha-gadgil/LeanAide.git" @ "mathlib4"
 
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
-require «doc-gen4» from git "https://github.com/siddhartha-gadgil/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4.git" @ "main"
