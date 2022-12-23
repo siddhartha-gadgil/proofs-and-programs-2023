fe094f0d504d65fbedc3fbbcaf595e828a1000fb
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-12-23 08:44:28 +0530

    corrected mk_imports.sh

--- mk_imports.sh
+++ mk_imports.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-
 find PnP2023 -name "*.lean" | env LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /' > PnP2023.lean
-printf '\ndef hello := "world"\n' > PnP2023.lean
+printf '\ndef hello := "world"\n' >> PnP2023.lean
