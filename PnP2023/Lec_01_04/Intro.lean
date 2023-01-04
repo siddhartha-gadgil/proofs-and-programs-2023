baf820c7dc678cca6a5e3a435b9fc5432aca385a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-01-04 10:02:57 +0530

    lecture live

--- PnP2023/Lec_01_04/Intro.lean
+++ PnP2023/Lec_01_04/Intro.lean
@@ -1,3 +1,4 @@
+import Mathlib
 /-!
 # Welcome to the course
 
@@ -16,3 +17,45 @@ We will then see
 /-!
 ## Lean as a calculator.
 -/
+#eval 1 + 2
+
+#eval "Hello " ++ "world!"
+
+def some_number := 42
+
+#eval some_number + 23
+
+/-! We next define some functions -/
+def add_two (n : ℕ) : ℕ := n + 2
+
+def cube (n : ℕ) : ℕ := n * n * n
+
+#eval cube (add_two 3)
+
+def cube' := fun (n : ℕ) ↦ n * n * n
+
+def cube'' : ℕ → ℕ := fun n ↦ n * n * n
+
+example := λ (n : ℕ) => n * n * n
+
+/-! Terms in Lean, including functions, have types, which can be seen using `#check` -/
+
+#check 1 + 2
+
+#check "Hello " ++ "world!"
+
+#check add_two
+#check cube 
+
+#check ℕ
+#check Type 
+#check ℕ → ℕ
+
+def sum_of_squares (x y : ℕ) : ℕ := x * x + y * y
+
+#check sum_of_squares -- ℕ → (ℕ → ℕ)
+
+#check sum_of_squares 3 -- ℕ → ℕ
+
+def sum_of_squares' : ℕ → ℕ → ℕ := 
+  fun x ↦ fun y ↦  x * x + y * y
