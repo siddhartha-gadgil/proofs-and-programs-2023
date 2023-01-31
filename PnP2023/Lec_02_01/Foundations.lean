3e35ab228ea3ce135003deda9e778afa2def4715
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-01-31 08:31:27 +0530

    foundations file

---
+++ PnP2023/Lec_02_01/Foundations.lean
@@ -0,0 +1,9 @@
+/-!
+# Foundations
+
+We have seen all the ingredients of the foundations of mathematics as implemented in Lean. Our goal now is to:
+
+* Systematically review them.
+* Put them in the context of what we mean by foundations of mathematics.
+* Separate the foundations from the syntax parsing and elaboration which help us work efficiently with Lean.
+-/
