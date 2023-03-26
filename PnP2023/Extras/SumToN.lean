9d95a5396c018b9a26ed0d27c52cd446794cd1aa
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-03-26 13:51:00 +0530

    eg tweaks

--- PnP2023/Extras/SumToN.lean
+++ PnP2023/Extras/SumToN.lean
@@ -4,10 +4,13 @@ def sumTo (n: ℕ)  := match n with
   | 0 => 0
   | n + 1 => (n + 1) + sumTo n
 
-theorem sum_formula (n: ℕ) : 2 * sumTo n = n * (n + 1):= by
+theorem sum_formula (n: ℕ) : 2 * sumTo n = n * (n + 1):= 
+  by -- 2 * sumTo n = n * (n + 1)
   induction n with
-  | zero => 
-    simp [sumTo]
+  | zero => -- 2 * sumTo Nat.zero = Nat.zero * (Nat.zero + 1)
+    rfl
   | succ n ih => 
-    simp [sumTo, ih]
+    simp [sumTo] -- 2 * (n + 1 + sumTo n) = Nat.succ n * (Nat.succ n + 1)
+    simp [left_distrib, ih] 
+    -- 2 * n + 2 + (n * n + n) = Nat.succ n * Nat.succ n + Nat.succ n
     linarith
